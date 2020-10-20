775099de7ba2eac5c2bc1c9dbecd7ed4b8555102
137303553322 <165002952@qq.com>
2020-10-20 11:02:22 +0800

    提交src

---
+++ src/main/java/com/example/testwarehouse/TestwarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.testwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestwarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestwarehouseApplication.class, args);
+    }
+
+}
